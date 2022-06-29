335b82200cd79b1b6d8301ee95b97b4268d16153
D-Pats <devinpatel97@gmail.com>
2022-06-29 18:59:26 +0100

    Logging created maven and added dependencies, instantiated logger

---
+++ Logging/src/main/java/Logging/LoggingTest.java
@@ -0,0 +1,16 @@
+package Logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingTest {
+	
+	public static Logger LOGGER = LogManager.getLogger();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
