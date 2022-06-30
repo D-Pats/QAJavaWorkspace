d04b6aa0421c5aa1174fdbf2ffae45c4d2aefc41
D-Pats <devinpatel97@gmail.com>
2022-06-30 15:26:43 +0100

    Updated with exercises for Streams

--- Streams/src/streams/Runner.java
+++ Streams/src/streams/Runner.java
@@ -50,32 +50,101 @@ public class Runner {
 //		    System.out.println(even);
 //
 ////			------------------------------------------
+//		
+//		List<String> teamNames = new ArrayList<String>();
+//		
+//		teamNames.add("RedBull");
+//		teamNames.add("McLaren");
+//		teamNames.add("Ferrari");
+//		teamNames.add("Mercedes");
+//		teamNames.add("AlphaTauri");
+//		
+//		long countMoreThanSeven = teamNames.stream().filter(str -> str.length() > 7).count();
+//		System.out.println(countMoreThanSeven + "with more than 7 chars in name.");
+//		
+//		List<String> mTeamNames = teamNames.stream()
+//				.filter(str -> str.startsWith("M"))
+//				.collect(Collectors.toList());
+//		
+//		System.out.println("Names starting with M:");
+//		mTeamNames.forEach(n -> System.out.println(n));
+//		
+//		List <String> sortedTeamNames = teamNames.stream()
+//				.sorted()
+//				.collect(Collectors.toList());
+//		
+//		System.out.println("List after sorting: ");
+//		sortedTeamNames.forEach(n -> System.out.println(n));
+//
+//			------------------------------------------------
 		
-		List<String> teamNames = new ArrayList<String>();
+		List<String> names = new ArrayList<String>();
 		
-		teamNames.add("RedBull");
-		teamNames.add("McLaren");
-		teamNames.add("Ferrari");
-		teamNames.add("Mercedes");
-		teamNames.add("AlphaTauri");
+		names.add("Michael");
+		names.add("Dean");
+		names.add("James");
+		names.add("Chris");
 		
-		long countMoreThanSeven = teamNames.stream().filter(str -> str.length() > 7).count();
-		System.out.println(countMoreThanSeven + "with more than 7 chars in name.");
+		names.forEach(n -> System.out.println("Hello "+ n));
 		
-		List<String> mTeamNames = teamNames.stream()
-				.filter(str -> str.startsWith("M"))
-				.collect(Collectors.toList());
+//		List<Integer> numbers = new ArrayList<Integer>();
+//		
+//		numbers.add(3);
+//		numbers.add(4);
+//		numbers.add(7);
+//		numbers.add(8);
+//		numbers.add(12);
 		
-		System.out.println("Names starting with M:");
-		mTeamNames.forEach(n -> System.out.println(n));
+		List<Integer> numbers = Arrays.asList(3,4,7,12,8);
 		
-		List <String> sortedTeamNames = teamNames.stream()
+		int numbersProduct = numbers.stream()
+				.reduce((a,b) -> a*b).get();
+		
+		System.out.println("Product of numbers: " + numbersProduct);
+		
+		
+		List<Integer> numbersOrdered = numbers.stream()
 				.sorted()
 				.collect(Collectors.toList());
 		
-		System.out.println("List after sorting: ");
-		sortedTeamNames.forEach(n -> System.out.println(n));
+		int numbersMax = numbersOrdered.get(numbersOrdered.size()-1);
+		System.out.println("Max number: " + numbersMax);
 		
+		int numbersMin = numbersOrdered.get(0);
+		System.out.println("Min number: " + numbersMin);
+		
+	    List<Integer> odd = 
+	            numbers.stream()
+	                .filter(x -> x % 2 != 0)
+	                .collect(Collectors.toList());
+		
+	    System.out.println("Odd numbers: " + odd);
+	    System.out.println("Odd numbers also below:");
+	    odd.forEach(n -> System.out.println(n));
+	    
+	    
+	    List<Integer> even = 
+	            numbers.stream()
+	                .filter(x -> x % 2 == 0)
+	                .collect(Collectors.toList());
+		
+	    System.out.println("Even numbers: " + even);
+	    System.out.println("Even numbers also below:");
+	    even.forEach(n -> System.out.println(n));
+		
+		int numbersSum = numbers.stream()
+				.reduce((a,b) -> a+b).get();
+		
+		System.out.println("Sum of numbers: "+ numbersSum);
+		
+		List<Integer> minEvenSquaredList =
+				numbers.stream()
+					.map(x -> x*x)
+					.filter(x -> x % 2 == 0)
+					.sorted()
+					.collect(Collectors.toList());
+		
+		System.out.println("Min even squared number: " + minEvenSquaredList.get(0));
 		    
 	}
 }
