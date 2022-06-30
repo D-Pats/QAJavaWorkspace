1c0b4c087624d8ad50bef80f4dc807ba4d8043c2
D-Pats <devinpatel97@gmail.com>
2022-06-30 11:53:18 +0100

    Updated Lambda Expressions

--- LambdaExpressions/src/lambdaExpressions/Runner.java
+++ LambdaExpressions/src/lambdaExpressions/Runner.java
@@ -2,21 +2,41 @@ package lambdaExpressions;
 
 import java.util.ArrayList;
 
-public class Runner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class Runner {
 		
-		ArrayList<Integer> listOne = new ArrayList<>();
+		interface FuncInterface {
+			int operation(int a, int b);}
+
+		private int operate(int a, int b, FuncInterface funcObj) {
+		    return funcObj.operation(a, b);
+		}
 		
-		listOne.add(2);
-		listOne.add(45);
-		listOne.add(23);
-		listOne.add(80);
-	
-		listOne.forEach(n -> {if (n > 25) System.out.println(n + " is larger than my age"); else System.out.println(n + " is equal to or smaller than my age");});
-		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			ArrayList<Integer> listOne = new ArrayList<>();
+
+			listOne.add(2);
+			listOne.add(45);
+			listOne.add(23);
+			listOne.add(80);
+
+			listOne.forEach(n -> {
+				if (n > 25)
+					System.out.println(n + " is larger than my age");
+				else
+					System.out.println(n + " is equal to or smaller than my age");
+			});
+
 //		listOne.forEach(n -> {(n <= 25) ? System.out.println(n + " is larger than my age") : System.out.println(n + " is equal to or smaller than my age")});
-	}
+//		-----------------------------------------
+			
+			FuncInterface multiply = (int x, int y) -> x * y;
+
+			Runner test = new Runner();
+			System.out.println(test.operate(listOne.get(0), listOne.get(2), multiply));
+
+		}
 
 }
